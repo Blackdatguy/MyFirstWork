be1870386600b3508266f762beec794eb90d2c0c
Blackdatguy <73775114+Blackdatguy@users.noreply.github.com>
2020-11-01 01:37:40 -0800

    Add files via upload
    My First Day works Java

---
+++ MyClass.java
@@ -0,0 +1,28 @@
+
+public class MyClass {
+	//create two variable
+	public int n1;
+	public int n2;
+	
+	//create the constructor
+	public MyClass(int a,int b) {
+		this.n1=a;
+		this.n2=b;
+	}
+
+	//create a method Add
+	public void add() {
+		System.out.println(n1+n2);
+		
+		
+	}
+
+	//create substract methos
+	public void sub() {
+		System.out.println(n1-n2);
+		
+	}
+
+
+}
+
---
+++ MyClass2.java
@@ -0,0 +1,24 @@
+
+public class MyClass2 {
+	
+	public static void main(String[] args) {
+	
+	//display string 
+	
+	System.out.println(".....Crating object from MyClass ...");
+	
+	
+	//create a object
+	
+	MyClass o1 =new MyClass(20,10);
+	
+	//call add method to o1
+	o1.add();
+	o1.sub();
+	
+	
+	
+	}
+	
+	
+}
